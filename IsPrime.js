5972e3854869079d8b524ed9e03ff6bc510fa8c0
sidraj2002 <siddharth.rajguru2013@gmail.com>
2016-12-30 16:50:23 -0600

    Simple IsPrime example

---
+++ IsPrime.js
@@ -0,0 +1,31 @@
+/*var n = 77;
+
+function prime(x, n){
+  if(x % n == 0){
+    return true;
+  } else return false;
+};
+function iterations(n){
+  for (var x=2; x*x <= n; x++ ){
+var isPrime = function prime(x, n)
+console.log(x);
+console.log(isPrime)
+}
+return isPrime;
+}
+var ifPrime = function iteratations(n)
+console.log(ifPrime);
+*/
+
+var n = 37;
+var isPrime= 'false';
+for(x=2; x*x <= n; x++){
+console.log(x)
+console.log(isPrime)
+if(n % x === 0){
+  isPrime = 'false'
+  break
+}else { isPrime = 'true'}
+
+}
+console.log(isPrime)
